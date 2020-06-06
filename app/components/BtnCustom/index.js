5aca3a3e93ca35fc4d99a42ccf580f9ddb1de5a5
Denny Rachmadi <denirachmadi82@gmail.com>
2020-06-06 23:21:19 +0800

    create custom Button

---
+++ app/components/BtnCustom/index.js
@@ -0,0 +1,35 @@
+/**
+ *
+ * PrimaryBtn
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import { typography } from '../../styles/constants';
+
+const StyledBtn = styled(props=>(
+    <Button {...props} />
+))`
+  && {
+    margin-top: 10px;
+    margin-bottom:10px;
+    font-family: ${typography.fontFamily};
+    font-weight: bold;
+    text-transform: capitalize;
+    box-shadow: none;
+  }`;
+
+const BtnCustom = ({ title, ...btnProps }) => {
+  return (
+    <StyledBtn {...btnProps}>{title}</StyledBtn>
+  );
+}
+
+BtnCustom.propTypes = {
+  title:PropTypes.string.isRequired
+};
+
+export default BtnCustom;
