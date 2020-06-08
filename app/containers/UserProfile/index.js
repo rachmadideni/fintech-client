84653887d3fe078c6ec7e617f421faa9a6fd20c8
Denny Rachmadi <denirachmadi82@gmail.com>
2020-06-08 15:47:33 +0800

    enhance profile page

--- app/containers/UserProfile/index.js
+++ app/containers/UserProfile/index.js
@@ -5,150 +5,115 @@
  */
 
 import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
 import injectSaga from 'utils/injectSaga';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-import makeSelectUserProfile from './selectors';
-import {
-  makeSelectEmail,
-  makeSelectNik,
-  makeSelectNotelp
-} from '../App/selectors';
-import messages from './messages';
-
-import {
-  removeAuthTokenAction
-} from '../App/actions';
-
-import saga from '../Login/saga';
-import { color, typography} from 'styles/constants';
-import waveBackground from '../../images/wave.svg';
-// import demimoore from '../../images/demimoore.jpg';
 
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import BtnCustom from 'components/BtnCustom';
 
-import styled from 'styled-components';
-const StyledPaper = styled(Paper)`
-&& {
-  background-image:url(${waveBackground});
-  background-size:cover;
-  margin-top:40px;  
-  padding:20px;
-  width:100vw;
-  // height:100vh;
-}`;
+import { typography } from 'styles/constants';
+import makeSelectUserProfile from './selectors';
+import messages from './messages';
+import { removeAuthTokenAction } from '../App/actions';
+
+import { makeSelectEmail, makeSelectNotelp } from '../App/selectors';
+import saga from '../Login/saga';
 
 const StyledAvatar = styled(Avatar)`
-&& {
-  margin-top:20px;
-  margin-bottom:10px;
-  width:80px;
-  height:80px;
-}`;
+  && {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    width: 80px;
+    height: 80px;
+  }
+`;
 
-const StyledText = styled(props=>{
-  return (
-    <Typography {...props} />
-  )
-})`
-&& {
-  font-family:${typography.fontFamily};
-  font-size:${props=>props.size}px;
-  font-weight:bold;
-}`;
-
-const StyledButton = styled(Button)`
-&& {
-  // width:150px;
-  margin-top:10px;
-  padding-left:20px;
-  padding-right:20px;
-  border-radius:18px;
-}
+const StyledText = styled(props => <Typography {...props} />)`
+  && {
+    font-family: ${typography.fontFamily};
+    font-size: ${props => props.size}px;
+    font-weight: bold;
+  }
 `;
 
 class UserProfile extends React.Component {
-  
-  goToChangePasswordPage = () => {
-    return this.props.history.replace('/changePassword');
-  }
+  goToChangePasswordPage = () => this.props.history.replace('/changePassword');
 
-  render(){
-    const { 
-      intl,
-      history,
-      removeAuthToken
-    } = this.props;
+  render() {
+    const { intl, removeAuthToken, email, notelp } = this.props;
     return (
-      <Grid 
-        container 
-        wrap="nowrap" 
-        direction="column"
-        style={{
-          alignItems:'center',          
-        }}>
-          <StyledPaper
-            elevation={0}>          
-            <Grid 
-              item>
-                <Grid 
-                  container 
-                  wrap="nowrap"
-                  direction="column"                  
-                  style={{
-                    justifyContent:'center',
-                    alignItems:'center',
-                    backgroundColor:'transparent'
-                  }}>
-                    <Grid item>
-                      <StyledAvatar />
-                    </Grid>
-                    <Grid item style={{ marginBottom:20,textAlign:'center' }}>
-                      <StyledText>MIA</StyledText>
-                      <StyledText size={12}>{this.props.email}</StyledText>
-                      <StyledText size={12}>{this.props.notelp}</StyledText>
-                    </Grid>
-                    <StyledButton 
-                      variant="outlined"
-                      color="secondary"
-                      onClick={this.props.removeAuthToken}>
-                      {intl.formatMessage(messages.logout)}
-                    </StyledButton>
-                    <StyledButton 
-                      variant="outlined"
-                      color="secondary"
-                      onClick={this.goToChangePasswordPage}>
-                      {intl.formatMessage(messages.ubahPassword)}
-                    </StyledButton>
-                </Grid>
-            </Grid>            
-          </StyledPaper>        
-      </Grid>
+      <Box
+        display="flex"
+        width="100%"
+        height="100%"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid
+          container
+          wrap="nowrap"
+          direction="column"
+          justify="center"
+          alignItems="center"
+          style={{ backgroundColor: 'transparent' }}
+        >
+          <Grid item>
+            <StyledAvatar />
+          </Grid>
+          <Grid item style={{ marginBottom: 20, textAlign: 'center' }}>
+            <StyledText>MIA</StyledText>
+            <StyledText size={12}>{email}</StyledText>
+            <StyledText size={12}>{notelp}</StyledText>
+          </Grid>
+
+          <BtnCustom
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            onClick={() => removeAuthToken()}
+            title={intl.formatMessage(messages.logout)}
+            style={{ borderRadius: '36px' }}
+          />
+
+          <BtnCustom
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            onClick={this.goToChangePasswordPage}
+            title={intl.formatMessage(messages.ubahPassword)}
+            style={{ borderRadius: '36px' }}
+          />
+        </Grid>
+      </Box>
     );
   }
 }
 
-// UserProfile.propTypes = {
-//   dispatch: PropTypes.func.isRequired,
-// };
+UserProfile.propTypes = {
+  intl: PropTypes.object,
+  history: PropTypes.object,
+  removeAuthToken: PropTypes.func,
+  email: PropTypes.string,
+  notelp: PropTypes.string,
+};
 
 const mapStateToProps = createStructuredSelector({
   userProfile: makeSelectUserProfile(),
   email: makeSelectEmail(),
-  notelp: makeSelectNotelp()  
+  notelp: makeSelectNotelp(),
 });
 
 function mapDispatchToProps(dispatch) {
   return {
-    // dispatch,
-    removeAuthToken: () => dispatch(removeAuthTokenAction())
+    removeAuthToken: () => dispatch(removeAuthTokenAction()),
   };
 }
 
@@ -157,7 +122,7 @@ const withConnect = connect(
   mapDispatchToProps,
 );
 
-const withSaga = injectSaga({ key:"loginSaga", saga });
+const withSaga = injectSaga({ key: 'loginSaga', saga });
 export default compose(
   withConnect,
   withSaga,
